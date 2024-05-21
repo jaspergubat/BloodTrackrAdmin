87931d4660e64984b32d9da626eaed9cd6ac2216
Jasper Gubat <jaspergubat02@gmail.com>
2024-05-21 18:23:24 -0300

    change

--- includes/header.php
+++ includes/header.php
@@ -28,7 +28,6 @@
       justify-content: flex-start;
       padding: 10px;
       margin: 5px 0;
-    
     }
     .form-inline {
       display: flex;
@@ -131,7 +130,50 @@
     <div class="content-container">
       <div id="dashboard" class="content">
         <h2>Dashboard</h2>
-        <p>Welcome to the dashboard.</p>
+        <div class="row">
+          <div class="col-lg-3 col-md-6">
+            <div class="panel panel-primary">
+              <div class="panel-heading">
+                <div class="row">
+                  <div class="col-xs-3">
+                    <i class="fa fa-user fa-5x"></i>
+                  </div>
+                  <div class="col-xs-9 text-right">
+                    <div class="huge">123</div>
+                    <div>Blood Banks</div>
+                  </div>
+                </div>
+              </div>
+              <div class="panel-footer" onclick="showPage('bloodBanks')">
+                <span class="pull-left">View Details</span>
+                <span class="pull-right"><i class="fa fa-arrow-circle-right"></i></span>
+                <div class="clearfix"></div>
+              </div>
+            </div>
+          </div>
+          <div class="col-lg-3 col-md-6">
+            <div class="panel panel-green">
+              <div class="panel-heading">
+                <div class="row">
+                  <div class="col-xs-3">
+                    <i class="fa fa-tasks fa-5x"></i>
+                  </div>
+                  <div class="col-xs-9 text-right">
+                    <div class="huge">12</div>
+                    <div>Users</div>
+                  </div>
+                </div>
+              </div>
+              <div class="panel-footer" onclick="showPage('users')">
+                <span class="pull-left">View Details</span>
+                <span class="pull-right"><i class="fa fa-arrow-circle-right"></i></span>
+                <div class="clearfix"></div>
+              </div>
+            </div>
+          </div>
+          <div class="col-lg-3 col-md-6"></div>
+          <div class="col-lg-3 col-md-6"></div>
+        </div>
       </div>
       <div id="bloodBanks" class="content">
         <h2>Blood Banks</h2>
